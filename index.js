43679e5cebb41739553cd5c09e50f2ae527efe44
N1koad <132302693+N1koad@users.noreply.github.com>
2023-05-02 05:53:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -88,9 +88,9 @@ app.get('/gpt/:text', async (req, res) => {
         messages.push({role: "assistant", content: agent_response})
 
         //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 500){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to first 500 characters.")
-          agent_response = agent_response.substring(0, 500)
+        if(agent_response.length > 2000){
+          console.log("Agent answer exceeds twitch chat limit. Slicing to first 2000 characters.")
+          agent_response = agent_response.substring(0, 2000)
           console.log ("Sliced agent answer: " + agent_response)
         }
 
